8dfa959efabde95b0f44d29f2779cfe5772cb79b
Mate Lakat <mlakat@suse.com>
2017-07-25 11:07:32 +0200

    neutron: Fix migrations
    We had multiple 55 migrations in the code. As a fix the newer 55
    migration 055_add_tunnel_csum has been merged to 56, so it will be
    executed.

--- chef/data_bags/crowbar/migrate/neutron/056_add_rpc_workers.rb
+++ chef/data_bags/crowbar/migrate/neutron/056_add_rpc_workers.rb
@@ -1,11 +1,21 @@
 def upgrade(ta, td, a, d)
   a["rpc_workers"] = ta["rpc_workers"] unless a.key?("rpc_workers")
 
+  # From 055_add_tunnel_csum.rb
+  a["ovs"] ||= {}
+  a["ovs"]["tunnel_csum"] = ta["ovs"]["tunnel_csum"]
+
+
   return a, d
 end
 
 def downgrade(ta, td, a, d)
   a.delete("rpc_workers")
+
+  # From 055_add_tunnel_csum
+  if a.key?("ovs")
+    a["ovs"].delete("tunnel_csum")
+  end
 
   return a, d
 end
